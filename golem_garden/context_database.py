e529dc811542557f75b36bf94f5ba5824b2a4bed
jonmatthis <jonmatthis@gmail.com>
2023-03-21 08:24:36 -0400

    gpt refactor context_database.py

--- golem_garden/context_database.py
+++ golem_garden/context_database.py
@@ -1,12 +1,11 @@
-import threading
 import json
+import threading
 from pathlib import Path
-from typing import List, Dict
+from typing import Dict, List
+
 
 class ContextDatabase:
-    def __init__(self,
-                 database_path: str = "user/context_database.json",
-                 user_id: str = "UnknownUser"):
+    def __init__(self, database_path: str = "user/context_database.json", user_id: str = "UnknownUser"):
         self._database = {}
         self.lock = threading.Lock()
         self.database_path = database_path
@@ -26,7 +25,6 @@ class ContextDatabase:
         if user_id not in self._database:
             self._database[user_id] = {}
 
-
     def add_message(self, golem_name: str, role: str, content: str):
         with self.lock:
             if golem_name not in self._database:
@@ -39,9 +37,12 @@ class ContextDatabase:
             return self._database[self._user_id].get(golem_id, [])
 
     def _save_to_file(self):
-        Path(self.database_path).parent.mkdir(parents=True, exist_ok=True)
+        self._create_parent_directory_if_not_exists()
         with open(self.database_path, 'w') as f:
             json.dump(self._database, f, indent=4)
+
+    def _create_parent_directory_if_not_exists(self):
+        Path(self.database_path).parent.mkdir(parents=True, exist_ok=True)
 
     def load_from_file(self):
         try:
@@ -56,34 +57,3 @@ class ContextDatabase:
 
     def print(self):
         print(self.as_json())
-
-
-if __name__ == "__main__":
-    context_database = ContextDatabase(user_id="test_user",
-                                       database_path="test_context_database.json")
-    context_database.load_from_file()
-    context_database.add_message(golem_name="test_golem",
-                                    role="user",
-                                    content="test message")
-    context_database.add_message(golem_name="test_golem",
-                                    role="assistant",
-                                    content="test response")
-    context_database.add_message(golem_name="test_golem",
-                                    role="user",
-                                    content="test message 2")
-    context_database.add_message(golem_name="test_golem",
-                                    role="assistant",
-                                    content="test response 2")
-    context_database.user_id = "test_user_2"
-    context_database.add_message(golem_name="test_golem",
-                                    role="user",
-                                    content="test message 3")
-    context_database.add_message(golem_name="test_golem",
-                                    role="assistant",
-                                    content="test response 3")
-    context_database.add_message(golem_name="test_golem",
-                                    role="user",
-                                    content="test message 4")
-
-
-    context_database.print()
