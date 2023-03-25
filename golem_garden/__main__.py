1230baadee90043a136292d441d3eba195219baa
jonmatthis <jonmatthis@gmail.com>
2023-03-25 14:46:10 -0400

    makeit works

--- golem_garden/__main__.py
+++ golem_garden/__main__.py
@@ -3,8 +3,6 @@ import click
 from golem_garden.user_interface.command_line_interface import chat_with_golem
 
 
-@click.command()
-@click.option('--cli', is_flag=True, help="Run the command line interface.")
 def main(cli: bool = True):
     if cli:
         chat_with_golem()
