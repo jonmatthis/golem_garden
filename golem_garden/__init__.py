c7f83b372d9952cb88e4f5ccb19bc2f1796bfab9
jonmatthis <jonmatthis@gmail.com>
2023-03-25 15:07:07 -0400

    fragamuffin devops

--- golem_garden/__init__.py
+++ golem_garden/__init__.py
@@ -1,3 +1,9 @@
+"""Welcome to the Garden - We're so glad you're here <3 """
 __version__ = "v0.4.0"
+import sys
+from pathlib import Path
+
+base_package_path = Path(__file__).parent.parent
+sys.path.insert(0, str(base_package_path))  # add parent directory to sys.path
 
 from golem_garden.user_interface.command_line_interface import chat_with_golem
