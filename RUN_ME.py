951104f7acf83f165af5c7a94470990e1dd95c15
jonmatthis <jonmatthis@gmail.com>
2023-03-29 19:55:59 -0400

    update deps and add CONTRIBUTING.md

--- RUN_ME.py
+++ RUN_ME.py
@@ -1,4 +1,5 @@
-from golem_garden.user_interface.command_line_interface import chat_with_golem
+from golem_garden.__main__ import enter_the_garden
+
 
 if __name__ == '__main__':
-    chat_with_golem()
+    enter_the_garden()
--- golem_garden/__main__.py
+++ golem_garden/__main__.py
@@ -2,7 +2,7 @@ from golem_garden.user_interface.command_line_interface import chat_with_golem
 from golem_garden.user_interface.jupyter_widget import run_qtconsole
 
 
-def main(cli: bool = False):
+def enter_the_garden(cli: bool = False):
     if cli:
         chat_with_golem()
     else:
@@ -10,4 +10,4 @@ def main(cli: bool = False):
 
 
 if __name__ == "__main__":
-    main()
+    enter_the_garden()
