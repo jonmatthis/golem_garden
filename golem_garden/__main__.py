f17f5b1f54d4f058e76eb3dcf87157694d791a1b
Jon Matthis <jonmatthis@gmail.com>
2023-03-29 10:52:28 -0400

    add as main entry point

--- golem_garden/__main__.py
+++ golem_garden/__main__.py
@@ -1,9 +1,12 @@
 from golem_garden.user_interface.command_line_interface import chat_with_golem
+from golem_garden.user_interface.jupyter_widget import run_qtconsole
 
 
-def main(cli: bool = True):
+def main(cli: bool = False):
     if cli:
         chat_with_golem()
+    else:
+        run_qtconsole()
 
 
 if __name__ == "__main__":
