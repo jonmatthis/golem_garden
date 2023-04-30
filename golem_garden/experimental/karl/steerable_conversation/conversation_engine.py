43e52de4d3d38e84ad4554c4b0b545d146e2751d
jonmatthis <jonmatthis@gmail.com>
2023-04-30 15:38:51 -0400

    update discord bot

--- golem_garden/experimental/karl/steerable_conversation/conversation_engine.py
+++ golem_garden/experimental/karl/steerable_conversation/conversation_engine.py
@@ -12,23 +12,44 @@ load_dotenv()
 
 import toml
 
-from typing import Dict, List, Any, Callable
+from typing import Callable
 
 from langchain.chat_models import ChatOpenAI
 
-agent_enpisi_config_path = str(Path(__file__).parent.parent.parent / "jkl" / "agent_definition" / "enpisi.config")
+agent_enpisi_config_path = str(Path(__file__).parent.parent.parent / "jkl" / "agent_definitions" / "enpisi.config")
+agent_list = [agent_enpisi_config_path, agent_enpisi_config_path]
+
+
+def agent_from_config_path(config_path, llm):
+    agent_definition = toml.load(config_path)
+
+    agent = NPCBuilderGPT.from_llm(llm, verbose=False, agent_config=agent_definition)
+    agent.seed_agent()
+
+    return agent
+
+
+def poll_func():
+    user_input = input("Enter your response, or 'QUIT' to cancel:")
+
+    return user_input
+
+
+def publish_func(msg):
+    print(msg)
+
 
 class ConversationEngine:
     def __init__(self,
-                 poll_func: Callable[[], int],
-                 publish_func: Callable[[int], None],
-                 agents: list,
-                 model_name  = 'gpt-3.5-turbo'):
+                 poll_func: Callable[[], int] = poll_func,
+                 publish_func: Callable[[int], None] = publish_func,
+                 agents: list = agent_list,
+                 model_name='gpt-3.5-turbo'):
 
         self.agents = agents
         llm = ChatOpenAI(model=model_name, temperature=0.9)
 
-        self.agent_1, self.agent_2 = [agent_from_config_path(path, llm = llm)  for path in agents]
+        self.agent_1, self.agent_2 = [agent_from_config_path(path, llm=llm) for path in self.agents]
 
         self.publish = publish_func
 
@@ -37,7 +58,7 @@ class ConversationEngine:
     def begin(self):
 
         while True:
-            
+
             message_1 = self.agent_1.step()
             self.publish(message_1)
             self.agent_2.input_step(message_1)
@@ -45,39 +66,22 @@ class ConversationEngine:
             message_2 = self.agent_2.step()
             self.publish(message_2)
             self.agent_1.input_step(message_2)
-               
+
             human_response = self.poll()
-            if (human_response == 'QUIT') or (human_response == 'quit') or (human_response == 'q') or (human_response == 'Q'):
+
+            if (human_response == 'QUIT') or (human_response == 'quit') or (human_response == 'q') or (
+                    human_response == 'Q'):
                 break
-            elif human_response=='':
+            elif human_response == '':
                 pass
             else:
                 self.agent_1.input_step(human_response)
                 self.agent_2.input_step(human_response)
 
 
-def agent_from_config_path(config_path, llm):
-    path = os.path.dirname(os.path.realpath(__file__)) + config_path
-    agent_definition=toml.load(path)
-
-    agent = NPCBuilderGPT.from_llm(llm, verbose=False, agent_config=agent_definition)
-    agent.seed_agent()
-
-    return agent
-
-def poll_func():
-
-    user_input = input("Enter your response, or 'QUIT' to cancel:")
-
-    return user_input
-
-def publish_func(msg):
-    print(msg)
-
-
 if __name__ == '__main__':
     conversation_engine = ConversationEngine(poll_func,
                                              publish_func,
-                                             agents=[ agent_enpisi_config_path, agent_enpisi_config_path],
+                                             agents=agent_list,
                                              model_name='gpt-3.5-turbo')
     conversation_engine.begin()
