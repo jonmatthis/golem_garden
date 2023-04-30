f0b0f437a5cbe23527cf09333fcc39afa4b3c430
jonmatthis <jonmatthis@gmail.com>
2023-04-30 09:00:34 -0400

    discord bot working

--- golem_garden/frontend/discord_bot/bot_main.py
+++ golem_garden/frontend/discord_bot/bot_main.py
@@ -1,16 +1,25 @@
-import discord
+import logging
 import os
+
+import discord
 from dotenv import load_dotenv
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
+
 bot = discord.Bot()
 
 @bot.event
 async def on_ready():
+    logger.info("Bot is ready!")
     print(f"{bot.user} is ready and online!")
+
 
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
+    logger.info(f"Received hello command: {ctx}")
     await ctx.respond("Hey!")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+if __name__ == "__main__":
+    bot.run(os.getenv("DISCORD_TOKEN"))
