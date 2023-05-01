df1f44a627d7b2d3ffc0f65c60af20848b7d407d
jonmatthis <jonmatthis@gmail.com>
2023-04-30 23:39:23 -0400

    working without prompt

--- golem_garden/backend/langchain_stuff/agents/agent_builder.py
+++ golem_garden/backend/langchain_stuff/agents/agent_builder.py
@@ -1,13 +1,19 @@
 import logging
+import os
 from pathlib import Path
 from typing import Union
 
 import toml
-from langchain import PromptTemplate
+from langchain import PromptTemplate, LLMChain, SerpAPIWrapper
+from langchain.agents import Tool, initialize_agent, AgentType
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory, ConversationSummaryMemory
 
 logger = logging.getLogger(__name__)
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class AgentBuilder:
@@ -17,6 +23,14 @@ class AgentBuilder:
         self._name = self._config["name"]
         self._llm = self._configure_llm()
         self._prompt = self._create_prompt()
+        # self._llm_chain = self._make_llm_chain()
+        self._memory = self._configure_memory()
+        self._tools = self._configure_tools()
+        self._agent_chain = self._make_agent_chain()
+
+    def run(self, message: str):
+        logger.info(f"Received message: {message}")
+        return self._agent_chain.run(message)
 
     def _configure_llm(self):
         llm_config = self._config["llm"]
@@ -26,26 +40,76 @@ class AgentBuilder:
 
     def _create_prompt(self):
         prompt_config = self._config["prompt"]
-        return PromptTemplate(
+
+        inputs = list(prompt_config["input_vars"].keys())
+        inputs.append("history")
+        inputs.append("human_input")
+
+        prompt = PromptTemplate(
             template=prompt_config["template"],
-            input_variables=["input_language", "output_language"],
+            input_variables=inputs,
         )
 
-    # def configure_memory(self):
-    #     memory_config = self._config["memory"]
-    #     self._golem._memory = ConversationBufferMemory(memory_key=memory_config["memory_key"],
-    #                                                    return_messages=memory_config["return_messages"])
-    #     return self
-    #
-    #
-    # def build(self):
-    #     self._golem._chain = initialize_agent(self._golem._tools,
-    #                                           self._golem._llm,
-    #                                           agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-    #                                           verbose=True,
-    #                                           memory=self._golem._memory)
-    #     return self._golem
+        prompt = prompt.partial(**prompt_config["input_vars"])
+
+        logger.info(f"Prompt: {prompt}")
+
+        return prompt
+
+    def _configure_memory(self):
+
+        memory_config = self._config["memory"]
+        logger.info(f"Configuring memory of type {memory_config}")
+
+        if memory_config["type"] == "ConversationBufferMemory":
+            memory = ConversationBufferMemory(memory_key=memory_config["memory_key"],
+                                              return_messages=memory_config["return_messages"])
+        elif memory_config["type"] == "ConversationBufferWindowMemory":
+            memory = ConversationBufferWindowMemory(memory_key=memory_config["memory_key"],
+                                                    return_messages=memory_config["return_messages"])
+        elif memory_config["type"] == "ConversationSummaryMemory":
+            memory = ConversationSummaryMemory(memory_key=memory_config["memory_key"],
+                                               return_messages=memory_config["return_messages"])
+        else:
+            raise NotImplementedError(f"Memory type {memory_config['type']} not implemented... YET!")
+
+        return memory
+
+    def _make_llm_chain(self):
+        llm_chain = LLMChain(llm=self._llm, prompt=self._prompt, verbose=True)
+        return llm_chain
+
+    def _configure_tools(self):
+        self._serper_search = SerpAPIWrapper(serpapi_api_key=os.environ["SERPER_API_KEY"])
+        tools_config = self._config["tools"]
+        tools = [
+            Tool(
+                name="Current Search",
+                func=self._serper_search.run,
+                description="useful for when you need to answer questions about current events or the current state of the world"
+            ),
+        ]
+        return tools
+
+    # def _make_agent(self):
+    #     agent_chain = initialize_agent(tools=self._tools,
+    #                                    llm= self.llm,
+    #                                    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+    #                                    verbose=True,
+    #                                    memory= self._memory)
+    def _make_agent_chain(self):
+
+        agent_chain = initialize_agent(tools=self._tools,
+                                       llm=self._llm,
+                                       agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
+                                       memory=self._memory,
+                                       verbose=True, )
+        return agent_chain
+
+
 
 
 if __name__ == "__main__":
-    builder = AgentBuilder(config_file="./configuration_tomls/dunkthulu.toml")
+    agent = AgentBuilder(config_file="./configuration_tomls/dunkthulu.toml")
+    print(agent.run("Hello, Tell me about youreself!"))
+    f = 9
