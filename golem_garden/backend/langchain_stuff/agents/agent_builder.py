8cce6a0bc6aa3672b9a62b1f1c9d53aa8f1c69e7
jonmatthis <jonmatthis@gmail.com>
2023-05-01 08:50:25 -0400

    hello Dunkthulu, ntmy

--- golem_garden/backend/langchain_stuff/agents/agent_builder.py
+++ golem_garden/backend/langchain_stuff/agents/agent_builder.py
@@ -8,7 +8,6 @@ from langchain import PromptTemplate, LLMChain, SerpAPIWrapper
 from langchain.agents import Tool, initialize_agent, AgentType
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory, ConversationSummaryMemory
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
 
 logger = logging.getLogger(__name__)
 
@@ -21,19 +20,20 @@ class AgentBuilder:
     def __init__(self,
                  config_file: Union[str, Path]):
         self._config = toml.load(config_file)
-        self._name = self._config["name"]
+        self._name = self._config["prompt"]["input_variables"]["name"]
         self._llm = self._configure_llm()
         self._prompt = self._create_prompt()
-        self._llm_chain = self._make_llm_chain()
+        # self._llm_chain = self._make_llm_chain()
         self._memory = self._configure_memory()
-        self._tools = self._configure_tools()
-        self._agent_chain = self._make_agent_chain()
+        self._llm_chain = LLMChain(llm=self._llm,
+                                   prompt=self._prompt,
+                                   memory=self._memory,
+                                   verbose=True)
 
 
-    def run(self, message: str):
+    def intake_message(self, message: str):
         logger.info(f"Received message: {message}")
-        return self._agent_chain.run(message)
-
+        return self._llm_chain.predict(human_input=message)
     def _configure_llm(self):
         llm_config = self._config["llm"]
         if llm_config["type"] == "ChatOpenAI":
@@ -42,20 +42,18 @@ class AgentBuilder:
 
     def _create_prompt(self):
         prompt_config = self._config["prompt"]
+        input_variables = list(prompt_config["input_variables"].keys())
+        input_variables.append("chat_history")
+        input_variables.append("human_input")
+        # input_variables.append("agent_scratchpad")
 
-
-        # human_message_prompt = HumanMessagePromptTemplate(prompt=prompt)
-        human_message_prompt = HumanMessagePromptTemplate(
-            prompt=PromptTemplate(
-                template=prompt_config["template"],
-                input_variables=list(prompt_config["input_vars"].keys()),
-            )
+        prompt_template = PromptTemplate(
+            template=prompt_config["template"],
+            input_variables=input_variables,
         )
-        human_message_prompt_formatted = human_message_prompt.format(**prompt_config["input_vars"])
-        chat_prompt_template = ChatPromptTemplate.from_messages([human_message_prompt_formatted])
+        prompt_formatted = prompt_template.partial(**prompt_config["input_variables"])
 
-
-        return chat_prompt_template
+        return prompt_formatted
 
     def _configure_memory(self):
 
@@ -81,37 +79,11 @@ class AgentBuilder:
 
         return llm_chain
 
-    def _configure_tools(self):
-        self._serper_search = SerpAPIWrapper(serpapi_api_key=os.environ["SERPER_API_KEY"])
-        tools_config = self._config["tools"]
-        tools = [
-            Tool(
-                name="Current Search",
-                func=self._serper_search.run,
-                description="useful for when you need to answer questions about current events or the current state of the world"
-            ),
-        ]
-        return tools
-
-    # def _make_agent(self):
-    #     agent_chain = initialize_agent(tools=self._tools,
-    #                                    llm= self.llm,
-    #                                    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-    #                                    verbose=True,
-    #                                    memory= self._memory)
-    def _make_agent_chain(self):
-
-        agent_chain = initialize_agent(tools=self._tools,
-                                       llm=self._llm,
-                                       agent=AgentType.CONVERSATIONAL_REACT_DESCRIPTION,
-                                       memory=self._memory,
-                                       verbose=True, )
-        return agent_chain
-
-
-
 
 if __name__ == "__main__":
     agent = AgentBuilder(config_file="./configuration_tomls/dunkthulu.toml")
-    print(agent.run("Hello, Tell me about youreself!"))
-    f = 9
+    print(agent.intake_message("Hello, tell me about yourself!"))
+    print(agent.intake_message("Thats wild, tell me more!"))
+    print("Done!")
+
+
